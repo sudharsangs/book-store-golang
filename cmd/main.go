23b6b644d1144a2ad214a79033702514b3c8fa58
sudharsangs <g.s.sudharsan99@gmail.com>
2022-03-12 16:56:06 +0530

    dump commit

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"book-store/pkg/routes"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":6969", r))
+	fmt.Println("Server running in PORT 6969")
+}
